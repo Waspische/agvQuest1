9b54df802b1db5245a0ff9965e70994a16fea9b1
astadler <adrien.stadler@gmail.com>
2021-03-24 22:12:08 +0100

    init 2

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container fill-height>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Quest nº1'
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="10" md="4">
+      <div class="text-center">
+        <v-img
+          :src="require('@/assets/pink_rabbit.jpeg')"
+          max-height="300"
+          contain
+        />
+      </div>
+    </v-col>
+    <v-col cols="12" sm="12" md="8">
+      <v-card>
+        <v-card-title class="headline">
+          How to escape your nightmare?
+        </v-card-title>
+        <v-card-text>
+          <p class="justify">
+            <code>It's time! It's time! Easter is here and I want my eggs.</code>
+          </p>
+          <p class="justify">
+            Your walk in the forest just turned into your worst nightmare, the Big Pink Bunny... What happened? You don't know. But he seems to be hungry. You may want to do what he wants.
+          </p>
+          <p class="justify">
+            <code>Oh dear! Oh dear! You shall be too late! Tell me more and jump.</code>
+          </p>
+          <v-divider />
+          <v-row>
+            <v-col
+              cols="12"
+              md="4"
+            >
+              <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+              >
+                <v-text-field
+                  v-model="discordName"
+                  :rules="[rules.required]"
+                  label="Discord name"
+                  placeholder="eg. Wasp#1975"
+                />
+              </v-form>
+            </v-col>
+            <v-col
+              cols="12"
+              md="4"
+            >
+              <v-btn
+                class="ma-2 primary"
+                large
+                @click="onStart"
+              >
+                Talk and enter the Pink Rabbit Hole
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      valid: true,
+      discordName: '',
+      rules: {
+        required: value => !!value || 'Required.'
+      }
+    }
+  },
+  methods: {
+    async onStart (e) {
+      // send request to join the game
+
+      if (this.$refs.form.validate()) {
+        try {
+          const data = await this.$axios.$post('http://localhost:8080/api/quest/rabbit',
+            {
+              name: this.getUsername(),
+              response: this.discordName
+            })
+          this.$router.push(data.next)
+          console.log(this.discordName)
+          this.setUsername(this.discordName)
+        } catch (e) {
+          this.discordName = ''
+        }
+      }
+    },
+    getUsername () {
+      const username = JSON.parse(localStorage.getItem('username')) || this.discordName
+      return username
+    },
+    setUsername (username) {
+      localStorage.setItem('username', JSON.stringify(username))
+    }
+  }
+}
+</script>
